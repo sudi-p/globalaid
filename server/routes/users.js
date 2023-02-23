78b4010d377e784ca3a8f43eca40537a85f70315
Sudip Paudel <spaudel645@outlook.com>
2023-02-23 16:40:00 -0500

    data from backend

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,7 +2,8 @@ import express from "express";
 import {
     getUser,
     createJob,
-    getJobs
+    getJobs,
+    getRentals
 } from "../controllers/users.js";
 import { verifyToken } from "../middleware/auth.js";
 
@@ -11,6 +12,7 @@ const router = express.Router();
 
 router.get("/getuser/", verifyToken, getUser);
 router.get("/getjobs/", verifyToken, getJobs);
+router.get("/getrentals/", verifyToken, getRentals)
 router.post("/createjob/", verifyToken, createJob);
 
 export default router;
