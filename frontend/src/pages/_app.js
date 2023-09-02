06a1c688518b6a128babd68a547f56b6107d4dac
unknown <spaudel645@gmail.com>
2023-09-02 17:32:48 -0400

    updated folder structure

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -4,7 +4,7 @@ import store from '../store/store';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import reportWebVitals from '../src/reportWebVitals';
+import reportWebVitals from '../reportWebVitals';
 import '../styles/globals.css';
 
 const theme = createTheme({
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./features/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./shared/**/*.{js,ts,jsx,tsx}",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   corePlugins: {
