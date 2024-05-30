09752a2c3dfea392f501e006ff7389b30fb1055f
unknown <spaudel645@gmail.com>
2024-05-30 14:44:48 -0400

    Making Empty Page UI better

--- frontend/src/components/chat/EmptyChat.tsx
+++ frontend/src/components/chat/EmptyChat.tsx
@@ -1,3 +1,4 @@
+import { Badge } from "@mui/material";
 import React from "react";
 import { FaComments } from "react-icons/fa";
 import { MdChatBubbleOutline } from "react-icons/md";
@@ -6,15 +7,16 @@ const NoChats = () => {
   return (
     <div className="flex flex-col items-center justify-center my-32 w-full bg-gray-100">
       <div className="flex flex-col items-center p-6 bg-white shadow-md border border-solid border-gray-300 rounded-lg">
-        <MdChatBubbleOutline className="text-gray-400 text-6xl mb-4" />
-        <h2 className="text-2xl font-bold text-gray-700 mb-2">
+        <Badge badgeContent={"0"} color="primary">
+          <MdChatBubbleOutline className="text-gray-400 text-6xl mb-4" />
+        </Badge>
+        <h2 className="text-3xl font-bold text-gray-700 mb-2">
           No Chats Available
         </h2>
-        <p className="text-gray-500 mb-4">
-          You don't have any conversations yet. You can message the ad owner
-          from the ad page.
+        <p className="text-2xl text-gray-500 mb-4 text-center">
+          You don't have any conversations yet.
+          <br /> You can message the ad owner from the ad page.
         </p>
-        <FaComments className="text-blue-500 text-5xl" />
       </div>
     </div>
   );
