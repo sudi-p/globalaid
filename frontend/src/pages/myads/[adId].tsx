5db3138c5499909460b369a3148b294d6f1b001c
unknown <spaudel645@gmail.com>
2024-06-02 02:16:11 -0400

    My Ad- Incomplete Ad

--- frontend/src/pages/myads/[adId].tsx
+++ frontend/src/pages/myads/[adId].tsx
@@ -1,10 +1,17 @@
-import React, { ReactElement, useEffect, useRef } from "react";
+import React, { ReactElement, ReactNode } from "react";
 import { useRouter } from "next/router";
 import { axiosPrivate } from "../../lib/api";
 import PageNotFound from "@pages/404";
 import NavbarLayout from "@components/layout/navBarLayout";
 import { useQuery } from "@tanstack/react-query";
 import Rental from "@src/components/myads/Rental";
+import { Link } from "@mui/material";
+import { FaPhone, FaEnvelope, FaHome, FaBriefcase } from "react-icons/fa";
+import {
+  Home as HomeIcon,
+  Engineering as EngineeringIcon,
+  Close as CloseIcon,
+} from "@mui/icons-material";
 
 export default function MyAd() {
   const { query } = useRouter();
@@ -25,15 +32,61 @@ export default function MyAd() {
   if (error) return <PageNotFound />;
   if (isLoading) return <div>Loading...</div>;
   const {
-    ad: { title, description, email, phone, company, adType },
+    ad: { title, description, complete, email, phone, company, adType },
   } = data;
   console.log(data);
   return (
-    <div className="max-w-screen-xl m-auto p-5">
-      <div className="text-3xl">{title}</div>
-      <div className="whitespace-pre-wrap">{description}</div>
-      {phone} | {email}
-      {adType == "rent" ? <Rental {...data} /> : <div>Job</div>}
+    <div className="max-w-screen-xl mx-auto p-6 bg-white shadow-lg rounded-lg">
+      <div className="flex justify-between">
+        <h2 className="font-bold mb-6 text-gray-800">{title}</h2>
+        {adType == "rent" ? (
+          <AdTypeButton text="Rental" icon={<HomeIcon />} />
+        ) : (
+          <AdTypeButton text="Job Vacancy" icon={<EngineeringIcon />} />
+        )}
+      </div>
+      <div className="text-base mb-8 whitespace-pre-wrap text-gray-700">
+        <p className="font-semibold">Description:</p>
+        {description}
+      </div>
+      <div>
+        Ad Type: <span className="capitalize"> {adType}</span>
+      </div>
+
+      {complete ? (
+        <div className="border-t pt-6">
+          <div className="flex items-center space-x-6 text-gray-600 mb-6">
+            <div className="flex items-center space-x-2">
+              <FaPhone className="text-gray-500" />
+              <span className="font-medium">{phone}</span>
+            </div>
+            <div className="flex items-center space-x-2">
+              <FaEnvelope className="text-gray-500" />
+              <span className="font-medium">{email}</span>
+            </div>
+          </div>
+          {adType === "rent" ? (
+            <Rental {...data} />
+          ) : (
+            <div className="flex items-center space-x-2 text-xl font-semibold text-gray-700">
+              <FaBriefcase />
+              <span>Job Details</span>
+            </div>
+          )}
+        </div>
+      ) : (
+        <div className="text-xl text-gray-500 mt-4 border border-dotted border-gray-300 w-max p-8 m-auto center">
+          This ad is incomplete. <br />
+          Please
+          <Link
+            href={`/myads/createad/${adId}`}
+            className="text-blue-500 underline mx-1"
+          >
+            click here
+          </Link>{" "}
+          to complete the ad.
+        </div>
+      )}
     </div>
   );
 }
@@ -41,3 +94,18 @@ export default function MyAd() {
 MyAd.getLayout = function getLayout(page: ReactElement) {
   return <NavbarLayout>{page}</NavbarLayout>;
 };
+
+type AdTypeButtonProps = {
+  icon: ReactNode;
+  text: string;
+};
+
+function AdTypeButton({ icon, text }: AdTypeButtonProps) {
+  return (
+    <div
+      className={`border border-solid flex items-center p-6 rounded cursor-pointer text-md border-green-300 w-max text-green-300 text-xl font-bold`}
+    >
+      {icon} <span className="ml-1">{text}</span>
+    </div>
+  );
+}
